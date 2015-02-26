ba5b73a6666e4c1492c9470d762c2254f710e41a
Andrew Kvalheim <Andrew@Kvalhe.im>
2015-02-25 17:17:23 -0800

    Don't override Middleman instantiation. Resolves #1.

--- lib/middleman-pry.rb
+++ lib/middleman-pry.rb
@@ -4,20 +4,10 @@ module Middleman
   module Cli
     # Override the built-in console command
     class Console
-      def console
-        require 'middleman-core'
+      def interact_with(context)
         require 'pry'
 
-        environment = options['environment']
-        verbose     = options['verbose']
-
-        @app = ::Middleman::Application.server.inst do
-          set :environment, environment.to_sym if environment
-
-          ::Middleman::Logger.singleton(verbose ? 0 : 1, false)
-        end
-
-        @app.pry
+        context.pry
       end
     end
   end
